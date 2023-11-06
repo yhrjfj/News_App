2e8288197f218a14ccd6387d375855930851cf04
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-11-06 17:04:02 +0600

    Splash Screen

--- app/src/main/java/com/yhrjfj/newsapp/MainActivity.kt
+++ app/src/main/java/com/yhrjfj/newsapp/MainActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.yhrjfj.newsapp.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Splash screen
+        installSplashScreen()
+
         setContent {
             NewsAppTheme {
                 Text(text = "YHRJFJ")
